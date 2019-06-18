886c30935eb2b6aa620c611706ff248edf9027b1
Blake Rose <bdecktor@gmail.com>
2019-06-18 16:17:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   extend Memorable::ClassMethods
 include Memorable::InstanceMethods
   extend Findable::ClassMethods
-  include Paramable 
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
